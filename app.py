7e15ed881a194ad89c4b0a7c17568a50afe84764
VignanBaligari234 <114463516+VignanBaligari234@users.noreply.github.com>
2023-04-01 12:22:13 +0530

    updating changes

--- app.py
+++ app.py
@@ -7,8 +7,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     urls = get_urls()
-    name = 'John Doe'
-    return render_template('index.html', urls=urls, name=name)
+    return render_template('index.html', urls=urls)
 
 if __name__ == '__main__':
     app.run()
